b414d290892dd759ee6f026d1dca369a19fe8168
puripatwo <125193942+puripatwo@users.noreply.github.com>
2025-03-28 03:50:59 +0000

    Updated train_textual_inversion.py

--- prompt_sliders/trainscripts/textsliders/train_textual_inversion.py
+++ prompt_sliders/trainscripts/textsliders/train_textual_inversion.py
@@ -8,7 +8,7 @@ import PIL
 from pathlib import Path
 from typing import List, Optional
 from packaging import version
-from tqdm.auto import tdqm
+from tqdm.auto import tqdm
 
 import numpy as np
 import safetensors
@@ -25,9 +25,6 @@ from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
 from huggingface_hub import create_repo, upload_folder
 
-if is_wandb_available():
-    import wandb
-
 import diffusers
 from diffusers import (
     AutoencoderKL,
@@ -40,6 +37,9 @@ from diffusers.optimization import get_scheduler
 from diffusers.utils import check_min_version, is_wandb_available
 from diffusers.utils.hub_utils import load_or_create_model_card, populate_model_card
 from diffusers.utils.import_utils import is_xformers_available
+
+if is_wandb_available():
+    import wandb
 
 if version.parse(version.parse(PIL.__version__).base_version) >= version.parse("9.1.0"):
     PIL_INTERPOLATION = {
@@ -129,17 +129,17 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    # 1. Check if WandB is available.
+    # Check if WandB is available
     if args.report_to == "wandb" and args.hub_token is not None:
         raise ValueError(
             "You cannot use both --report_to=wandb and --hub_token due to a security risk of exposing your token."
-            " Please use `huggingface-cli login` to authenticate with the Hub."
+            "Please use `huggingface-cli login` to authenticate with the Hub."
         )
     if args.report_to == "wandb":
         if not is_wandb_available():
             raise ImportError("Make sure to install wandb if you want to use it for logging during training.")
     
-    # 2. Set up Accelerator.
+    # Set up Accelerator
     logging_dir = os.path.join(args.output_dir, args.logging_dir)
     accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, logging_dir=logging_dir)
     accelerator = Accelerator(
@@ -149,7 +149,7 @@ if __name__ == "__main__":
         project_config=accelerator_project_config,
     )
 
-    # 3. Make one log on every process with the configuration for debugging.
+    # Make one log on every process with the configuration for debugging
     logging.info(accelerator.state, main_process_only=False)
     if accelerator.is_local_main_process:
         transformers.utils.logging.set_verbosity_warning()
@@ -158,18 +158,18 @@ if __name__ == "__main__":
         transformers.utils.logging.set_verbosity_error()
         diffusers.utils.logging.set_verbosity_error()
     
-    # 4. If passed along, set the training seed now.
+    # If passed along, set the training seed now
     if args.seed is not None:
         set_seed(args.seed)
 
-    # 5. Handle the repository creation.
+    # Handle the repository creation
     if accelerator.is_main_process:
         if args.output_dir is not None:
             os.makedirs(args.output_dir, exist_ok=True)
         if args.push_to_hub:
             repo_id = create_repo(repo_id=args.hub_model_id or Path(args.output_dir).name, exist_ok=True, token=args.hub_token).repo_id
         
-    # 6. Load the pre-trained models.
+    # 1. Load the pre-trained models.
     if args.tokenizer_name:
         tokenizer = CLIPTokenizer.from_pretrained(args.tokenizer_name)
     elif args.pretrained_model_name_or_path:
@@ -180,4 +180,50 @@ if __name__ == "__main__":
     unet = UNet2DConditionModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="unet", revision=args.revision, variant=args.variant)
     vae = AutoencoderKL.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae", revision=args.revision, variant=args.variant)
 
+    ### TEXTUAL INVERSION ###
+    # 2. Add the placeholder token in tokenizer.
+    placeholder_tokens = [args.placeholder_token]
+
+    if args.num_vectors < 1:
+        raise ValueError(f"--num_vectors has to be larger or equal to 1, but is {args.num_vectors}.")
+    
+    # 2.1. Add additional tokens for each vector.
+    additional_tokens = []
+    for i in range(1, args.num_vectors):
+        additional_tokens.append(f"{args.placeholder_token}_{i}")
+    placeholder_tokens += additional_tokens
+
+    # 2.2. Add the placeholder token to the tokenizer.
+    num_added_tokens = tokenizer.add_tokens(placeholder_tokens)
+    if num_added_tokens != args.num_vectors:
+        raise ValueError(
+            f"The tokenizer already contains the token {args.placeholder_token}. Please pass a different"
+            " `placeholder_token` that is not already in the tokenizer."
+        )
+    
+    # 2.3. Convert the initializer and placeholder tokens to IDs.
+    token_ids = tokenizer.encode(args.initializer_token, add_special_tokens=False)
+    if len(token_ids) > 1:
+        raise ValueError("The initializer token must be a single token.")
+    
+    initializer_token_id = token_ids[0]
+    placeholder_token_ids = tokenizer.convert_tokens_to_ids(placeholder_tokens)
+
+    # 2.4. Resize the token embeddings of the text encoder.
+    text_encoder.resize_token_embeddings(len(tokenizer))
+
+    # 2.5. Initialise the newly added placeholder token with the embeddings of the initializer token.
+    token_embeds = text_encoder.get_input_embeddings().weight.data
+    with torch.no_grad():
+        for placeholder_token_id in placeholder_token_ids:
+            token_embeds[placeholder_token_id] = token_embeds[initializer_token_id].clone()
+
+    # Freeze vae and unet
+    vae.requires_grad_(False)
+    unet.requires_grad_(False)
+
+    # Freeze all parameters except for the token embeddings in text encoder
+    text_encoder.text_model.encoder.requires_grad_(False)
+    text_encoder.text_model.final_layer_norm.requires_grad_(False)
+    text_encoder.text_model.embeddings.position_embedding.requires_grad_(False)
     
