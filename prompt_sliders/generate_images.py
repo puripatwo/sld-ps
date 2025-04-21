5ec538e78bdc972e0164e814a4980e2ae2c28819
puripatwo <125193942+puripatwo@users.noreply.github.com>
2025-04-21 04:45:45 +0100

    Add generate_image.py and image_editing.py for Prompt Sliders

---
+++ prompt_sliders/generate_images.py
@@ -0,0 +1,173 @@
+import torch
+import numpy as np
+import matplotlib.image as mpimg
+import copy
+import gc
+import argparse
+import os, json, random, sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob, re
+import warnings
+warnings.filterwarnings("ignore")
+from typing import Any, Dict, List, Optional, Tuple, Union
+from PIL import Image
+from tqdm import tqdm
+
+import safetensors.torch
+from safetensors.torch import load_file
+from transformers import CLIPTextModel, CLIPTokenizer
+
+import diffusers
+from diffusers import DiffusionPipeline
+from diffusers import AutoencoderKL, DDPMScheduler, DiffusionPipeline, UNet2DConditionModel, LMSDiscreteScheduler
+from diffusers.loaders import AttnProcsLayers
+from diffusers.models.attention_processor import LoRAAttnProcessor, AttentionProcessor
+
+def flush():
+    torch.cuda.empty_cache()
+    gc.collect()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', help='Name of the model.', type=str, required=True)
+    parser.add_argument('--learned_embeds_path', help='Path to the learned embeddings.', type=str, required=True)
+    parser.add_argument('--placeholder_token', help='The placeholder token to be used.', type=str, default="iid-1")
+    parser.add_argument('--save_path', help='Folder of where to save the images.', type=str, required=True)
+    args = parser.parse_args()
+
+    model_name = args.model_name
+    learned_embeds_path = args.learned_embeds_path
+    placeholder_token = args.placeholder_token
+    save_path = args.save_path
+
+    # 1. Create directories for each scale.
+    flush()
+    weight_dtype = torch.float32
+    scales = [0, 0.5, 1.0, 1.5]
+    for scale in scales:
+        os.makedirs(os.path.join(save_path, os.path.basename(model_name), os.path.basename(scale)), exist_ok=True)
+
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:0")
+    else:
+        device = "cpu"
+
+    # 2. Load in the scheduler, tokenizer, and models.
+    revision = None
+    pretrained_model_name_or_path = "runwayml/stable-diffusion-v1-5"
+    noise_scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+    tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_name_or_path, subfolder="tokenizer", revision=revision)
+    text_encoder = CLIPTextModel.from_pretrained(pretrained_model_name_or_path, subfolder="text_encoder", revision=revision)
+    vae = AutoencoderKL.from_pretrained(pretrained_model_name_or_path, subfolder="vae", revision=revision)
+    unet = UNet2DConditionModel.from_pretrained(pretrained_model_name_or_path, subfolder="unet", revision=revision)
+
+    # 3. Check if the token already exists in the vocabulary.
+    placeholder_token = f"<{placeholder_token}>"
+    if placeholder_token not in tokenizer.get_vocab():
+        tokenizer.add_tokens([placeholder_token])
+        placeholder_token_id = tokenizer.convert_tokens_to_ids(placeholder_token)
+        text_encoder.resize_token_embeddings(len(tokenizer))
+    
+    # 4. Inject the learned embeddings in the position of the placeholder token id.
+    loaded_embeds = safetensors.torch.load_file(learned_embeds_path)
+    key = list(loaded_embeds.keys())[0]
+    new_token_embed = loaded_embeds[key]
+    with torch.no_grad():
+        text_encoder.get_input_embeddings().weight.data[placeholder_token_id] = new_token_embed.clone()
+
+    unet.requires_grad_(False)
+    unet.to(device, dtype=weight_dtype)
+    vae.requires_grad_(False)
+    vae.to(device, dtype=weight_dtype)
+    text_encoder.requires_grad_(False)
+    text_encoder.to(device, dtype=weight_dtype)
+
+    prompts = [
+        f"a photo of a man, <{placeholder_token}>",
+        ]
+    start_noise = 800
+    num_images_per_prompt = 1
+    torch_device = device
+    negative_prompt = None
+    batch_size = 1
+    height = 512
+    width = 512
+    ddim_steps = 50
+    guidance_scale = 7.5
+
+    # 5. Loop through each prompt.
+    for case_number, prompt in enumerate(prompts):
+        for num in range(num_images_per_prompt):
+            seed = random.randint(0, 5000)
+
+            print(prompt, seed)
+
+            # 6. Loop through each scale.
+            for scale in scales:
+                generator = torch.manual_seed(seed)
+
+                # 6.1. Tokenize the prompt.
+                text_input = tokenizer(prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
+                max_length = text_input.input_ids.shape[-1]
+                batch_indices = torch.arange(len(text_input.input_ids))
+                idx = text_input.input_ids.argmax(-1)
+
+                # 6.2. Prepare the unconditional embeddings.
+                if negative_prompt is None:
+                    uncond_input = tokenizer([""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt")
+                else:
+                    uncond_input = tokenizer([negative_prompt] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt")
+                uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
+
+                # 6.3. Prepare timesteps and latent variables.
+                latents = torch.randn((batch_size, unet.in_channels, height // 8, width // 8), generator=generator)
+                latents = latents.to(torch_device)
+                noise_scheduler.set_timesteps(ddim_steps)
+                latents = latents * noise_scheduler.init_noise_sigma
+                latents = latents.to(weight_dtype)
+
+                # 6.4. Denoising loop.
+                for t in tqdm(noise_scheduler.timesteps):
+                    # Prepare the text embeddings
+                    text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
+                    if t > start_noise and scale > 0.0:
+                        text_embeddings[batch_indices, idx, :] = 0.0 * text_embeddings[batch_indices, idx, :]
+                    else:
+                        text_embeddings[batch_indices, idx, :] = scale * text_embeddings[batch_indices, idx, :]
+                    
+                    # Prepare embeddings for classifier-free guidance
+                    concat_text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+                    concat_text_embeddings = concat_text_embeddings.to(weight_dtype)
+
+                    # Expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+                    latent_model_input = torch.cat([latents] * 2)
+                    latent_model_input = noise_scheduler.scale_model_input(latent_model_input, timestep=t)
+
+                    # Predict the noise residual
+                    with torch.no_grad():
+                        noise_pred = unet(latent_model_input, t, encoder_hidden_states=concat_text_embeddings).sample
+                    
+                    # Perform guidance
+                    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+                    # Compute the previous noisy sample x_t -> x_t-1
+                    latents = noise_scheduler.step(noise_pred, t, latents).prev_sample
+
+                # 6.5. Scale and decode the image latents with vae.
+                latents = 1 / 0.18215 * latents
+                with torch.no_grad():
+                    image = vae.decode(latents).sample
+
+                # 6.6. Image post-processing.
+                image = (image / 2 + 0.5).clamp(0, 1)
+                image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+                images = (image * 255).round().astype("uint8")
+                pil_images = [Image.fromarray(image) for image in images]
+
+                # 6.7. Loop through each generated image and store them.
+                for im in pil_images:
+                    image_filename = f"{case_number}_{num}.png"
+                    im.save(os.path.join(os.path.join(save_path, os.path.basename(model_name), os.path.basename(scale)), image_filename))
--- prompt_sliders/image_editing.py
+++ prompt_sliders/image_editing.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pandas as pd
 import argparse
-import os, json, random
+import os, json, random, sys
 import glob, re
 import copy
 import gc
@@ -12,11 +12,14 @@ import inspect
 import random
 import abc
 import shutil
+import warnings
+warnings.filterwarnings("ignore")
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from PIL import Image
 from tqdm import tqdm
 
 from transformers import CLIPTextModel, CLIPTextModelWithProjection, CLIPTokenizer
+import safetensors.torch
 from safetensors.torch import load_file
 from torch.optim.adam import Adam
 import torch.nn.functional as nnf
@@ -30,7 +33,6 @@ from diffusers.models.attention_processor import LoRAAttnProcessor, AttentionPro
 from diffusers.pipelines import StableDiffusionXLPipeline
 from diffusers.pipelines.stable_diffusion_xl import StableDiffusionXLPipelineOutput
 
-from trainscripts.textsliders.lora import LoRANetwork, DEFAULT_TARGET_REPLACE, UNET_TARGET_REPLACE_MODULE_CONV
 import trainscripts.textsliders.ptp_utils as ptp_utils
 
 LOW_RESOURCE = False
@@ -260,21 +262,26 @@ class NullInversion:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='editImages', description='Edit Images using Diffusers Code')
+    parser = argparse.ArgumentParser()
     parser.add_argument('--model_name', help='Name of the model.', type=str, required=True)
     parser.add_argument('--image_path', help='Path to the image to be edited.', type=str, required=True)
     parser.add_argument('--image_prompt', help='Prompt of the image to be edited.', type=str, required=True)
+    parser.add_argument('--learned_embeds_path', help='Path to the learned embeddings.', type=str, required=True)
+    parser.add_argument('--placeholder_token', help='The placeholder token to be used.', type=str, default="iid-1")
     parser.add_argument('--save_path', help='Folder of where to save the images.', type=str, required=True)
     args = parser.parse_args()
 
     model_name = args.model_name
     image_path = args.image_path
     image_prompt = args.image_prompt
+    learned_embeds_path = args.learned_embeds_path
+    placeholder_token = args.placeholder_token
     save_path = args.save_path
 
     # 1. Create directories to store the results.
+    flush()
     weight_dtype = torch.float32
-    scales = [0, 1, 2, 3]
+    scales = [0, 0.5, 1.0, 1.5]
     os.makedirs(os.path.join(save_path, os.path.basename(model_name), os.path.basename(image_path)), exist_ok=True)
 
     if torch.cuda.is_available():
@@ -301,12 +308,26 @@ if __name__ == "__main__":
 
     # 4. Load in the scheduler, tokenizer, and models.
     revision = None
-    pretrained_model_name_or_path = "CompVis/stable-diffusion-v1-4"
-    noise_scheduler = DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
+    pretrained_model_name_or_path = "runwayml/stable-diffusion-v1-5"
+    noise_scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
     tokenizer = CLIPTokenizer.from_pretrained(pretrained_model_name_or_path, subfolder="tokenizer", revision=revision)
     text_encoder = CLIPTextModel.from_pretrained(pretrained_model_name_or_path, subfolder="text_encoder", revision=revision)
     vae = AutoencoderKL.from_pretrained(pretrained_model_name_or_path, subfolder="vae", revision=revision)
     unet = UNet2DConditionModel.from_pretrained(pretrained_model_name_or_path, subfolder="unet", revision=revision)
+
+    # 5. Check if the token already exists in the vocabulary.
+    placeholder_token = f"<{placeholder_token}>"
+    if placeholder_token not in tokenizer.get_vocab():
+        tokenizer.add_tokens([placeholder_token])
+        placeholder_token_id = tokenizer.convert_tokens_to_ids(placeholder_token)
+        text_encoder.resize_token_embeddings(len(tokenizer))
+    
+    # 6. Inject the learned embeddings in the position of the placeholder token id.
+    loaded_embeds = safetensors.torch.load_file(learned_embeds_path)
+    key = list(loaded_embeds.keys())[0]
+    new_token_embed = loaded_embeds[key]
+    with torch.no_grad():
+        text_encoder.get_input_embeddings().weight.data[placeholder_token_id] = new_token_embed.clone()
 
     unet.requires_grad_(False)
     unet.to(device, dtype=weight_dtype)
@@ -315,84 +336,41 @@ if __name__ == "__main__":
     text_encoder.requires_grad_(False)
     text_encoder.to(device, dtype=weight_dtype)
 
-    rank = 4
-    alpha = 1
-    if 'full' in model_name:
-        train_method = 'full'
-    elif 'noxattn' in model_name:
-        train_method = 'noxattn'
-    elif 'xattn' in model_name:
-        train_method = 'xattn'
-    else:
-        train_method = 'noxattn'
-
-    network_type = "c3lier"
-    if train_method == 'xattn':
-        network_type = 'lierla'
-
-    modules = DEFAULT_TARGET_REPLACE
-    if network_type == "c3lier":
-        modules += UNET_TARGET_REPLACE_MODULE_CONV
-
-    # 5. Perform LoRA injection.
-    lora_weight = model_name
-    network = LoRANetwork(
-            unet,
-            rank=rank,
-            multiplier=1.0,
-            alpha=alpha,
-            train_method=train_method,
-        ).to(device, dtype=weight_dtype)
-    # network.load_state_dict(torch.load(lora_weight))
-    state_dict = load_file(lora_weight)
-    missing, unexpected = network.load_state_dict(state_dict, strict=False)
-    print("Missing keys:", missing)
-    print("Unexpected keys:", unexpected)
-
-    # # From the model currently in memory
-    # print("Expected keys:")
-    # for k in network.state_dict().keys():
-    #     print(k)
-
-    # # From the .safetensors file
-    # from safetensors.torch import load_file
-    # state_dict = load_file(lora_weight)
-
-    # print("\nLoRA file keys:")
-    # for k in state_dict.keys():
-    #     print(k)
-
-    width = 512
-    height = 512
-    ddim_steps = 50
-    guidance_scale = 7.5
+    start_noise = 800
+    num_images_per_prompt = 1
+    torch_device = device
     negative_prompt = None
     batch_size = 1
-    start_noise = 500
+    height = 512
+    width = 512
+    ddim_steps = 50
+    guidance_scale = 7.5
 
-    # 6
+    # 7. Loop through each scale.
     for scale in scales:
-        # 6.1. Tokenize the prompt.
+        # 7.1. Tokenize the prompt.
         text_input = tokenizer(image_prompt, padding="max_length", max_length=tokenizer.model_max_length, truncation=True, return_tensors="pt")
-        text_embeddings = text_encoder(text_input.input_ids.to(device))[0]
         max_length = text_input.input_ids.shape[-1]
+        batch_indices = torch.arange(len(text_input.input_ids))
+        idx = text_input.input_ids.argmax(-1)
 
-        # 6.2. Prepare timesteps and latent variables.
+        # 7.2. Prepare timesteps and latent variables.
         noise_scheduler.set_timesteps(ddim_steps)
         latents = x_t * noise_scheduler.init_noise_sigma
         latents = latents.to(unet.dtype)
 
-        # 6.3. Denoising loop.
+        # 7.3. Denoising loop.
         cnt = -1
         for t in tqdm(noise_scheduler.timesteps):
             cnt += 1
 
-            # LoRA slider is set dynamically based on timestep t
-            if t > start_noise:
-                network.set_lora_slider(scale=0)
+            # Prepare the text embeddings
+            text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
+            if t > start_noise and scale > 0.0:
+                text_embeddings[batch_indices, idx, :] = 0.0 * text_embeddings[batch_indices, idx, :]
             else:
-                network.set_lora_slider(scale=scale)
-
+                text_embeddings[batch_indices, idx, :] = scale * text_embeddings[batch_indices, idx, :]
+            
             # Prepare embeddings for classifier-free guidance
             concat_text_embeddings = torch.cat([uncond_embeddings_copy[cnt].expand(*text_embeddings.shape), text_embeddings])
             concat_text_embeddings = concat_text_embeddings.to(weight_dtype)
@@ -403,9 +381,8 @@ if __name__ == "__main__":
 
             # Predict the noise residual
             with torch.no_grad():
-                with network:
-                    noise_pred = unet(latent_model_input, t, encoder_hidden_states=concat_text_embeddings).sample
-
+                noise_pred = unet(latent_model_input, t, encoder_hidden_states=concat_text_embeddings).sample
+            
             # Perform guidance
             noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
             noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
@@ -413,18 +390,18 @@ if __name__ == "__main__":
             # Compute the previous noisy sample x_t -> x_t-1
             latents = noise_scheduler.step(noise_pred, t, latents).prev_sample
 
-        # 6.4. Scale and decode the image latents with vae.
+        # 7.5. Scale and decode the image latents with vae.
         latents = 1 / 0.18215 * latents
         with torch.no_grad():
             image = vae.decode(latents).sample
-        
-        # 6.5. Image post-processing.
+
+        # 7.6. Image post-processing.
         image = (image / 2 + 0.5).clamp(0, 1)
-        image = image.detach().cpu().permute(0, 2, 3, 1).to(torch.float16).numpy()
+        image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
         images = (image * 255).round().astype("uint8")
         pil_images = [Image.fromarray(image) for image in images]
 
-        # 6.6. Loop through each generated image and store them.
+        # 7.7. Loop through each generated image and store them.
         for im in pil_images:
             image_filename = f"{scale}.png"
-            im.save(os.path.join(os.path.join(save_path, os.path.basename(lora_weight), os.path.basename(image_path)), image_filename))
+            im.save(os.path.join(os.path.join(save_path, os.path.basename(model_name), os.path.basename(image_path)), image_filename))
